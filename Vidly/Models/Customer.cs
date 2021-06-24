fe239bfcf1cf78491d10a3ee6a0d68cca6c30294
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-06-24 13:01:28 +0530

    adding migration for model MembershipType also linking between customer and model

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -10,5 +10,7 @@ namespace Vidly.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSuscribedToNewsLetter { get; set; }
+        public MembershipType MembershipType { get; set; }//this property will help to have association with MembershipType class model
+        public byte MembershipTypeID { get; set; }//will be used as a forign key for MembershipType Id property
     }
 }
---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public short SignUpFee { get; set; }
+        public byte DurationInMonth { get; set; }
+        public byte DiscountRate { get; set; }
+        public byte ID {get; set;}
+
+        //Need to have an assosciation with customer class
+    }
+}
