f4d347e519d9e576784812c486af21e9585e7d6f
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-06-28 21:32:46 +0530

    Filled MembershipType database with set of values which is going to be common in prod,qa,dev etc

--- Vidly/Migrations/202106281553461_PopulateMembershipTypes.cs
+++ Vidly/Migrations/202106281553461_PopulateMembershipTypes.cs
@@ -7,7 +7,13 @@ namespace Vidly.Migrations
     {
         public override void Up()
         {
-
+            //here we can write sql query to put data into sql
+            //we are pushing data of membership type because it will be going to 
+            //consistent data over the qa dev and other environment
+            Sql("Insert into MembershipTypes(Id,SignupFee,DurationInMonth,DiscountRate) values (1,0,0,0)");//one time use member
+            Sql("Insert into MembershipTypes(Id,SignupFee,DurationInMonth,DiscountRate) values (2,30,1,10)");//weekly suscribed member
+            Sql("Insert into MembershipTypes(Id,SignupFee,DurationInMonth,DiscountRate) values (3,90,3,15)");//monthly suscribed member
+            Sql("Insert into MembershipTypes(Id,SignupFee,DurationInMonth,DiscountRate) values (4,300,12,20)");//yearly suscribed member
         }
         
         public override void Down()
