683741e517d009b1aa29ca02651d7713227c993d
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-06-29 15:10:16 +0530

    Changes in customer controller to load data from database and eager loading for related objects to load along with customer likke Membershiptype

--- Vidly/Controllers/CustomerController.cs
+++ Vidly/Controllers/CustomerController.cs
@@ -4,21 +4,39 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class CustomerController : Controller
     {
+        private ApplicationDbContext _context;
+        public CustomerController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //ApplicationDbContext is disposable object 
+        //so we need to properly dispose this object by overriding dispose method of base controller class
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customer
         public ViewResult Index()
         {
-            var customers = GetCustomers();
-            return View(customers);
+            //var customers = GetCustomers();
+            //var customer = _context.Customers.ToList();//but we want to load membershiptype data as well
+            //Eager loading
+            var customer = _context.Customers.Include(c => c.MembershipType).ToList();//Include() method is in sytem.data.entity
+            return View(customer);
         }
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            //var customer = GetCustomers().SingleOrDefault(c => c.Id == id);//old code
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
             {
                 return HttpNotFound();
@@ -35,6 +53,6 @@ namespace Vidly.Controllers
                 new Customer() {Name="Marvel " ,Id=3 },
                 new Customer() {Name="Johnson" ,Id=4 }
             };
-        }
+        }//we want to get rid of this method and want to fetch data from the databse
     }
 }
