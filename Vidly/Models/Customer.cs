eca182aa0fed6a998c105eaad3f99949615574ab
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-06-29 14:37:13 +0530

    Added data annotation to Name field of customer model class Added migration to the database

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,11 @@ namespace Vidly.Models
     public class Customer
     {
         public int Id { get; set; }
+        //we can apply data anotations to override default conventions of entity framework
+        //like by default entity framework will apply string name as nvarchar and nullable
+        //after making changes in data annotations we need to push those changes to table using migrations
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSuscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }//this property will help to have association with MembershipType class model
