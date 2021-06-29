e787e76a1efabe86243a0a1b3546fb469eb67dd1
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-06-29 21:54:12 +0530

    Updated membershiptype name as required annotation added migration and updated database

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,11 +8,13 @@ namespace Vidly.Models
 {
     public class MembershipType
     {
+        public byte ID { get; set; }
+        [Required]
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonth { get; set; }
         public byte DiscountRate { get; set; }
-        public byte ID {get; set;}
-        public string Name { get; set; }
+       
 
         //Need to have an assosciation with customer class
     }
