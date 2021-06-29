159599e9036510912eb3c86568fdacc08e215f3e
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-06-29 21:48:41 +0530

    updated datetime datatype of dateofbith of customer as nullable added migration and updated database

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -19,6 +19,6 @@ namespace Vidly.Models
         public MembershipType MembershipType { get; set; }//this property will help to have association with MembershipType class model
         public byte MembershipTypeID { get; set; }//will be used as a forign key for MembershipType Id property
 
-        public DateTime BirthDate { get; set; }
+        public DateTime? BirthDate { get; set; }
     }
 }
