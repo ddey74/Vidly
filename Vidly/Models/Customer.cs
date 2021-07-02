63ef059e153a7365b2491bf99842d58cf6f3bf92
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-07-02 22:19:28 +0530

    Change in customer model, added annotation to show membership type insted ofmembershiptypeId on the view

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -17,6 +17,7 @@ namespace Vidly.Models
         public string Name { get; set; }
         public bool IsSuscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }//this property will help to have association with MembershipType class model
+        [Display(Name="Membership Type")]
         public byte MembershipTypeID { get; set; }//will be used as a forign key for MembershipType Id property
 
         public DateTime? BirthDate { get; set; }
