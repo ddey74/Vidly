66f607d3fe6e64b488c688d787b4c7e34c6b5497
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-07-07 10:48:49 +0530

    Implemented Get list of customers from API

--- Vidly/Controllers/API/CustomersController.cs
+++ Vidly/Controllers/API/CustomersController.cs
@@ -4,10 +4,22 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Vidly.Models;
 
 namespace Vidly.Controllers.API
 {
     public class CustomersController : ApiController
     {
+        private ApplicationDbContext _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //GET: /api/customers
+        public IEnumerable<Customer> GetCustomer()
+        {
+            return _context.Customers.ToList();
+        
+        }
     }
 }
