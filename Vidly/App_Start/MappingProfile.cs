f804f6186e9b0c2d066f355d53915ecfdbc0be52
ddey74 <86369082+ddey74@users.noreply.github.com>
2021-07-07 21:48:08 +0530

    Configuration to use AutoMapper

---
+++ Vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Vidly.Models;
+using Vidly.DTOs;
+
+namespace Vidly.App_Start
+{
+    public class MappingProfile: Profile//From Automapper assembly
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+            //Also we need to initilize mapping on the application start event hence make changes in the global.asax.cs
+        }
+    }
+}
--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -6,6 +6,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Vidly.App_Start;
+using AutoMapper;
 
 namespace Vidly
 {
@@ -13,7 +15,8 @@ namespace Vidly
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());//Initilizing our user defined profile to use in Automapper
+            GlobalConfiguration.Configure(WebApiConfig.Register);//added to configure web api
 
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
